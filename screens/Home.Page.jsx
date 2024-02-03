607dacf5a60024a155f20fe5b44a34d6a13498c3
Salman-Ali05 <sallukhan0805gmail.com>
2024-02-03 22:43:38 +0100

    API Weather done

--- screens/Home.Page.jsx
+++ screens/Home.Page.jsx
@@ -1,10 +1,15 @@
-import { View, Text, StyleSheet, ImageBackground, Image, ScrollView } from 'react-native'
+import { View, Text, StyleSheet, ImageBackground, Image, ScrollView, TextInput } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import Icon from 'react-native-vector-icons/FontAwesome';
+import axios from 'axios';
 
 const HomePage = () => {
+
+    const APIKEYWEATHER = "5858c82ebf597ee396b0cbace54ddf20";
+
     const currentDate = new Date();
     const hour = currentDate.getHours();
+
     // Ok ici c'est GPT, j'avais la flemme de bien réfléchir sur ce cas là x')
     const getDaySuffix = (day) => {
         if (day >= 11 && day <= 13) {
@@ -39,9 +44,39 @@ const HomePage = () => {
 
     const days = ['Monday', 'Tuesday', 'Wednesday', 'Thrusday', 'Friday', 'Saturday', 'Sunday']
 
+    const [data, setData] = useState();
+    const [inputTown, setInputTown] = useState("Paris");
+
+    const fetchData = () => {
+        let config = {
+            method: 'GET',
+            maxBodyLength: Infinity,
+            url: "http://api.openweathermap.org/data/2.5/weather?q=" + inputTown + "&appid=" + APIKEYWEATHER
+        };
+
+        axios.request(config)
+            .then((response) => {
+                setData(response.data)
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        // console.log(data);
+    }
+
+    const [search, setSearch] = useState(false);
+
+    const handleSearch = () => {
+        setSearch(!search);
+    }
+
     useEffect(() => {
         setPic();
     }, [currentWeatherPic]);
+
+    useEffect(() => {
+        fetchData()
+    }, []);
 
     return (
         <View style={styles.container}>
@@ -49,26 +84,36 @@ const HomePage = () => {
                 <ImageBackground style={styles.currentWeather} source={currentWeatherPic}>
                     <View style={styles.topElements}>
                         <View>
-                            <Text style={[styles.currentDay, { color: textColor }]}>{formattedDay}</Text>
-                            <Image
-                                style={styles.heartList}
-                                source={
-                                    hour >= 17
-                                        ? require('../assets/heart_list_icon_white.png')
-                                        : require('../assets/heart_list_icon.png')
-                                }
-                            />
+                            {
+                                search ? (
+                                    <TextInput placeholder="Town's name" style={styles.textInput} onChangeText={(e) => setInputTown(e)} />
+                                ) : (
+                                    <React.Fragment>
+                                        <Text style={[styles.currentDay, { color: textColor }]}>{formattedDay}</Text>
+                                        <Image
+                                            style={styles.heartList}
+                                            source={
+                                                hour >= 17
+                                                    ? require('../assets/heart_list_icon_white.png')
+                                                    : require('../assets/heart_list_icon.png')
+                                            }
+                                        />
+                                    </React.Fragment>
+                                )
+
+                            }
                         </View>
                         <View>
-                            <Icon style={styles.searchIcon} name="search" />
-                            <Icon style={[styles.heartIcon, { color: textColor }]} name="heart-o" />
+                            <Icon style={styles.searchIcon} name="search" onPress={handleSearch} />
+                            <Icon style={[styles.heartIcon, { color: textColor }]} name="heart-o" onPress={fetchData} />
                         </View>
                     </View>
                     <View>
-                        <Text style={[styles.textsTopSide, { color: textColor }]}>Paris</Text>
-                        <Text style={[styles.textsTopSide, { color: textColor }]}>26°C</Text>
-                        <Text style={[styles.textsTopSide, { color: textColor }]}>Sunny</Text>
-                        <Text style={[styles.textsTopSide, { color: textColor }]}>↓19° ↑27°</Text>
+                        <Text style={[styles.textsTopSide, { color: textColor }]}>{data && data.name}</Text>
+                        <Text style={[styles.textsTopSide, { color: textColor }]}>{data && (data.main.temp - 273).toFixed(1)}°C</Text>
+                        <Text style={[styles.textsTopSide, { color: textColor }]}>{data && data.weather && data.weather[0] && data.weather[0].main}</Text>
+
+                        <Text style={[styles.textsTopSide, { color: textColor }]}>↓{data && (data.main.temp_min - 273).toFixed(0)}° ↑{data && (data.main.temp_max - 273).toFixed(0)}°</Text>
                     </View>
                 </ImageBackground>
             </View>
@@ -101,6 +146,7 @@ const styles = StyleSheet.create({
     },
     currentWeather: {
         flex: 1,
+        justifyContent: "space-around"
     },
     topElements: {
         marginTop: 30,
@@ -129,6 +175,17 @@ const styles = StyleSheet.create({
         fontSize: 32,
         textAlign: "center"
     },
+    textInput: {
+        alignSelf: "center",
+        color: "#333",
+        width: 300,
+        textAlign: "center",
+        backgroundColor: "#FFFFFF80",
+        height: 45,
+        borderRadius: 15,
+        fontSize: 20
+
+    },
     // START BOTTOM CSS
     bottomSide: {
         height: "40%",
@@ -136,9 +193,9 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     viewListDay: {
-        borderBottomWidth: 1,
         width: "80%",
         alignSelf: "center",
+        borderColor: "#333",
     },
     textsBottomSide: {
         fontSize: 20,
